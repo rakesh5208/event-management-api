510ce3230f54e6081c78f693eb7b2f81ec6c0089
=Rakesh Kumar Jha <rakesh5208@gmail.com>
2018-06-18 20:01:59 +0530

    removed copyrights text

--- src/main/java/com/learningwithrakesh/EventManagement/config/PersistentConfig.java
+++ src/main/java/com/learningwithrakesh/EventManagement/config/PersistentConfig.java
@@ -1,21 +1,3 @@
-/*******************************************************************************
- *  ============================================================================
- *                       Copyright (C) Pfizer Inc.
- *                               All rights reserved
- *  ============================================================================
- *  Notice: All Rights Reserved.
- *  This material contains the trade secrets and confidential information of
- *  Pfizer Inc., which embody substantial creative effort, ideas and
- *  expressions. No part of this material may be reproduced or transmitted
- *  in any form or by any means, electronic, mechanical, optical or otherwise,
- *  including photocopying and recording, or in connection with any information
- *  storage or retrieval system, without written permission from:
- *
- *  Hospira, a Pfizer company
- *  13520 Evening Creek Dr., Suite 200
- *  San Diego, CA 92128
- *  www.hospira.com
- *******************************************************************************/
 package com.learningwithrakesh.EventManagement.config;
 
 import java.util.Properties;
